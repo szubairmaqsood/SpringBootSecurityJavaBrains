21c647f259ef8a324d7055d91b4a1614e070e448
zubair-pc\zubair <szubair@gmail.com>
2021-05-17 17:36:33 +0500

    Notes authentication maaager authenticate in our web application 1. Through aurhntication manager  we added our users 2.Applied password encoder

---
+++ src/main/kotlin/com/example/SpringSecurityJavaBrains/security/SecurityConfigurtion.kt
@@ -0,0 +1,30 @@
+package com.example.SpringSecurityJavaBrains.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.crypto.password.NoOpPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfigurtion:WebSecurityConfigurerAdapter() {
+
+    override fun configure(auth: AuthenticationManagerBuilder?) {
+      auth?.inMemoryAuthentication()
+              ?.withUser("blah")
+              ?.password("blah")
+              ?.roles("USER")
+              ?.and()
+              ?.withUser("foo")
+              ?.password("foo")
+              ?.roles("ADMIN")
+    }
+
+    @Bean
+    fun getPasswordEncoder(): PasswordEncoder {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
